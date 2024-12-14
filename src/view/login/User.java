02c8cbb9d85976cfef5634c2bee1945846232473
NeilY <yang060315@163.com>
2024-12-14 21:37:27 +0800

    Update

--- src/view/login/User.java
+++ src/view/login/User.java
@@ -131,7 +131,7 @@ public class User {
     public static void initialUsers() {
         File file = new File(User.path);
         if (!file.exists()) {
-            System.out.println("正在初始化用户信息...");
+            System.out.println("正在初始化用户列表...");
             List<User> defaultUsers = Arrays.asList(
                     new User(0, "", "", createDefaultLv(false)),
                     new User(1, "admin", "64d09d9930c8ecf79e513167a588cb75439b762ce8f9b22ea59765f32aa74ca19d2f1e97dc922a3d4954594a05062917fb24d1f8e72f2ed02a58ed7534f94d27", createDefaultLv(true)),
